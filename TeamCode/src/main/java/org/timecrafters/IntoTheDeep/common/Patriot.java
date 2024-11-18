8f87317cf73dbe264760f3e86b90943e921ab657
SpencerPiha <88354753+SpencerPiha@users.noreply.github.com>
2024-11-18 03:36:31 -0600

    everything for patriot tele op, barely works

--- TeamCode/src/main/java/org/timecrafters/IntoTheDeep/common/Patriot.java
+++ TeamCode/src/main/java/org/timecrafters/IntoTheDeep/common/Patriot.java
@@ -1,10 +1,15 @@
 package org.timecrafters.IntoTheDeep.common;
 
+import com.qualcomm.hardware.digitalchickenlabs.OctoQuad;
+import com.qualcomm.hardware.digitalchickenlabs.OctoQuadBase;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.timecrafters.Library.Robot;
 
@@ -15,7 +20,16 @@ public class Patriot implements Robot {
     private HardwareMap hardwareMap;
     private String string;
     private CyberarmEngine engine;
-    public DcMotor fl, fr, bl, br;
+    public DcMotorEx fl, fr, bl, br, intakeExtendo, depositRightExtendo, depositLeftExtendo;
+    public CRServo leftDiff, rightDiff;
+    public Servo depoClaw, depoLeft, depoRight, intakeClaw;
+    public final int RIGHT_DIFFY = 1;
+    public final int LEFT_DIFFY  = 0;
+
+    // Declare the OctoQuad object and members to store encoder positions and velocities
+    public OctoQuad octoquad;
+    public int posLeftDiffy;
+    public int posRightDiffy;
     public IMU imu;
 
     public Patriot(String string) {
@@ -31,22 +45,34 @@ public class Patriot implements Robot {
         //      IMU
         imu = engine.hardwareMap.get(IMU.class, "imu_ex");
 
+        // OCTOQUAD
+        octoquad = engine.hardwareMap.get(OctoQuad.class, "octoquad");
+        octoquad.setSingleEncoderDirection(LEFT_DIFFY,  OctoQuad.EncoderDirection.FORWARD);
+        octoquad.setSingleEncoderDirection(RIGHT_DIFFY, OctoQuad.EncoderDirection.REVERSE);
+
         //motors configuration
-        fl = engine.hardwareMap.dcMotor.get("fl");
-        fr = engine.hardwareMap.dcMotor.get("fr");
-        br = engine.hardwareMap.dcMotor.get("br");
-        bl = engine.hardwareMap.dcMotor.get("bl");
+        intakeExtendo = (DcMotorEx) engine.hardwareMap.dcMotor.get("extension");
+        depositLeftExtendo = (DcMotorEx) engine.hardwareMap.dcMotor.get ("leftLift");
+        depositRightExtendo = (DcMotorEx) engine.hardwareMap.dcMotor.get ("rightLift");
 
-        fl.setDirection(DcMotorSimple.Direction.FORWARD);
-        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        depositLeftExtendo.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        fl = (DcMotorEx) engine.hardwareMap.dcMotor.get("fl");
+        fr = (DcMotorEx) engine.hardwareMap.dcMotor.get("fr");
+        br = (DcMotorEx) engine.hardwareMap.dcMotor.get("br");
+        bl = (DcMotorEx) engine.hardwareMap.dcMotor.get("bl");
+
+        fl.setDirection(DcMotorEx.Direction.FORWARD);
+        fl.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        fr.setDirection(DcMotorEx.Direction.REVERSE);
         fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        bl.setDirection(DcMotorSimple.Direction.FORWARD);
+        bl.setDirection(DcMotorEx.Direction.FORWARD);
         bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        br.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorEx.Direction.REVERSE);
         br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         IMU.Parameters parameters = new IMU.Parameters(
@@ -57,5 +83,24 @@ public class Patriot implements Robot {
 
         imu.initialize(parameters);
 
+        // SERVO'S
+        intakeClaw = engine.hardwareMap.servo.get("theClaw");
+        depoClaw = engine.hardwareMap.servo.get("depo claw");
+        depoLeft = engine.hardwareMap.servo.get("depo left");
+        depoRight = engine.hardwareMap.servo.get("depo right");
+        depoRight.setDirection(Servo.Direction.FORWARD);
+        depoLeft.setDirection(Servo.Direction.REVERSE);
+
+        // CR SERVO'S
+        leftDiff = engine.hardwareMap.crservo.get("leftDiff");
+        rightDiff = engine.hardwareMap.crservo.get("rightDiff");
+        leftDiff.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightDiff.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    public void readOctoQuad(){
+        int[] positions = octoquad.readAllPositions();
+        posLeftDiffy  = positions[LEFT_DIFFY];
+        posRightDiffy = positions[RIGHT_DIFFY];
     }
 }
--- TeamCode/src/main/java/org/timecrafters/IntoTheDeep/teleop/states/PatriotTeleOpState.java
+++ TeamCode/src/main/java/org/timecrafters/IntoTheDeep/teleop/states/PatriotTeleOpState.java
@@ -2,21 +2,38 @@ package org.timecrafters.IntoTheDeep.teleop.states;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.timecrafters.IntoTheDeep.common.Patriot;
+import com.acmerobotics.dashboard.config.Config;
 
 import dev.cyberarm.engine.V2.CyberarmState;
 
+@Config
 public class PatriotTeleOpState extends CyberarmState {
 
     private final Patriot robot;
     private double drivePower = 1;
+    private double maxVelocity = 2320 * 0.8;
     private boolean lbsVar;
+    private String armPos = "default";
+    public static double depoPivotPos = 0;
+    public double IntakeCLOSE = 1;
+    public double DepoCLOSE = 1;
+    public double IntakeOPEN = 0.4;
+    public double DepoOPEN = 0.7;
+    public double intakeOrigin = 0;
+    public double intakeExposed = 5100;
+    public double tolerance = 300;
+    public long lastExecutedTime;
+    public int differential;
+
 
     public PatriotTeleOpState(Patriot robot) {
         this.robot = robot;
     }
 
-    @Override
-    public void exec() {
+    public void DriveTrainControl() {
+
+
+        // -------------------------------------------------------------------------------------------------------------- Player 1 (Wheels)
 
         // reset orientation, and move camera when reset to see...
         if (engine.gamepad1.right_stick_button) {
@@ -46,17 +63,210 @@ public class PatriotTeleOpState extends CyberarmState {
         double rotX = x * Math.cos(heading) - y * Math.sin(heading);
         double rotY = x * Math.sin(heading) + y * Math.cos(heading);
 
-        double frontLeftPower = (rotY + rotX + rx) / denominator;
-        double backLeftPower = (rotY - rotX + rx) / denominator;
-        double frontRightPower = (rotY - rotX - rx) / denominator;
-        double backRightPower = (rotY + rotX - rx) / denominator;
+        double frontLeftPower = maxVelocity * ((rotY + rotX + rx) / denominator);
+        double backLeftPower = maxVelocity * ((rotY - rotX + rx) / denominator);
+        double frontRightPower = maxVelocity * ((rotY - rotX - rx) / denominator);
+        double backRightPower = maxVelocity * ((rotY + rotX - rx) / denominator);
 
         // setting each power determined previously from the math above
         // as well as multiplying it by a drive power that can be changed.
-        robot.bl.setPower(backLeftPower * drivePower);
-        robot.br.setPower(backRightPower * drivePower);
-        robot.fl.setPower(frontLeftPower * drivePower);
-        robot.fr.setPower(frontRightPower * drivePower);
+        robot.bl.setVelocity(backLeftPower * drivePower);
+        robot.br.setVelocity(backRightPower * drivePower);
+        robot.fl.setVelocity(frontLeftPower * drivePower);
+        robot.fr.setVelocity(frontRightPower * drivePower);
+    }
+
+    public void ArmPos(){
+        if (engine.gamepad2.a && !engine.gamepad2.left_bumper && !engine.gamepad2.right_bumper) {
+            armPos = "Default";
+        } else if (engine.gamepad2.a && engine.gamepad2.left_bumper && !engine.gamepad2.right_bumper){
+            armPos = "Transfer 1/2";
+        } else if (engine.gamepad2.a && engine.gamepad2.left_bumper && engine.gamepad2.right_bumper){
+            armPos = "Transfer 2/2";
+        } else if (engine.gamepad2.y && !engine.gamepad2.left_bumper){
+            armPos = "Deposit 1/2";
+        } else if (engine.gamepad2.y && engine.gamepad2.left_bumper) {
+            armPos = "Deposit 2/2";
+        } else if (engine.gamepad2.x){
+            armPos = "Intake";
+        } else if (engine.gamepad2.b){
+            armPos = "Reset";
+        }
+    }
+
+    public void ArmSequencer() {
+        if (armPos.equals("Default")) {
+            differential = 0;
+            robot.intakeClaw.setPosition(IntakeCLOSE);
+            robot.depoClaw.setPosition(DepoOPEN);
+//            robot.depoRight.setPosition(0);
+            robot.depoLeft.setPosition(0);
+            if (robot.posLeftDiffy > intakeOrigin + tolerance) {
+                robot.leftDiff.setPower(0.5);
+            } else if (robot.posLeftDiffy < intakeOrigin - tolerance) {
+                robot.leftDiff.setPower(-0.5);
+            } else {
+                robot.leftDiff.setPower(0);
+            }
+            if (robot.posRightDiffy > intakeOrigin + tolerance) {
+                robot.rightDiff.setPower(0.5);
+            } else if (robot.posRightDiffy < intakeOrigin - tolerance) {
+                robot.rightDiff.setPower(-0.5);
+            } else {
+                robot.rightDiff.setPower(0);
+            }
+
+        }
+        if (armPos.equals("Transfer 1/2")) {
+            differential = 0;
+            robot.intakeClaw.setPosition(IntakeCLOSE);
+            robot.depoClaw.setPosition(DepoCLOSE);
+//            robot.depoRight.setPosition(0);
+            robot.depoLeft.setPosition(0);
+            if (robot.posLeftDiffy > intakeOrigin + tolerance) {
+                robot.leftDiff.setPower(0.5);
+            } else if (robot.posLeftDiffy < intakeOrigin - tolerance) {
+                robot.leftDiff.setPower(-0.5);
+            } else {
+                robot.leftDiff.setPower(0);
+            }
+            if (robot.posRightDiffy > intakeOrigin + tolerance) {
+                robot.rightDiff.setPower(0.5);
+            } else if (robot.posRightDiffy < intakeOrigin - tolerance) {
+                robot.rightDiff.setPower(-0.5);
+            } else {
+                robot.rightDiff.setPower(0);
+            }
+
+        }
+        if (armPos.equals("Transfer 2/2")) {
+            differential = 0;
+            robot.intakeClaw.setPosition(IntakeOPEN);
+            robot.depoClaw.setPosition(DepoCLOSE);
+//            robot.depoRight.setPosition(0);
+            robot.depoLeft.setPosition(0);
+            if (robot.posLeftDiffy > intakeOrigin + tolerance) {
+                robot.leftDiff.setPower(0.5);
+            } else if (robot.posLeftDiffy < intakeOrigin - tolerance) {
+                robot.leftDiff.setPower(-0.5);
+            } else {
+                robot.leftDiff.setPower(0);
+            }
+            if (robot.posRightDiffy > intakeOrigin + tolerance) {
+                robot.rightDiff.setPower(0.5);
+            } else if (robot.posRightDiffy < intakeOrigin - tolerance) {
+                robot.rightDiff.setPower(-0.5);
+            } else {
+                robot.rightDiff.setPower(0);
+            }
+
+        }
+        if (armPos.equals("Deposit 1/2")) {
+            differential = 0;
+            robot.intakeClaw.setPosition(IntakeCLOSE);
+            robot.depoClaw.setPosition(DepoCLOSE);
+//            robot.depoRight.setPosition(1);
+            robot.depoLeft.setPosition(1);
+
+            if (robot.posLeftDiffy > intakeOrigin + tolerance) {
+                robot.leftDiff.setPower(0.5);
+            } else if (robot.posLeftDiffy < intakeOrigin - tolerance) {
+                robot.leftDiff.setPower(-0.5);
+            } else {
+                robot.leftDiff.setPower(0);
+            }
+            if (robot.posRightDiffy > intakeOrigin + tolerance) {
+                robot.rightDiff.setPower(0.5);
+            } else if (robot.posRightDiffy < intakeOrigin - tolerance) {
+                robot.rightDiff.setPower(-0.5);
+            } else {
+                robot.rightDiff.setPower(0);
+            }
+
+        }
+        if (armPos.equals("Deposit 2/2")) {
+            differential = 0;
+            robot.intakeClaw.setPosition(IntakeOPEN);
+            robot.depoClaw.setPosition(DepoOPEN);
+//            robot.depoRight.setPosition(1);
+            robot.depoLeft.setPosition(0);
+            if (robot.posLeftDiffy > intakeOrigin + tolerance) {
+                robot.leftDiff.setPower(0.5);
+            } else if (robot.posLeftDiffy < intakeOrigin - tolerance) {
+                robot.leftDiff.setPower(-0.5);
+            } else {
+                robot.leftDiff.setPower(0);
+            }
+            if (robot.posRightDiffy > intakeOrigin + tolerance) {
+                robot.rightDiff.setPower(0.5);
+            } else if (robot.posRightDiffy < intakeOrigin - tolerance) {
+                robot.rightDiff.setPower(-0.5);
+            } else {
+                robot.rightDiff.setPower(0);
+            }
+
+        }
+        if (armPos.equals("Intake")) {
+            robot.intakeClaw.setPosition(IntakeOPEN);
+            robot.depoClaw.setPosition(DepoOPEN);
+//            robot.depoRight.setPosition(0);
+            robot.depoLeft.setPosition(0);
+
+            if (robot.posLeftDiffy > intakeExposed + tolerance - differential) {
+                robot.leftDiff.setPower(0.5);
+            } else if (robot.posLeftDiffy < intakeExposed - tolerance - differential) {
+                robot.leftDiff.setPower(-0.5);
+            } else {
+                robot.leftDiff.setPower(0);
+            }
+            if (robot.posRightDiffy > intakeExposed + tolerance + differential) {
+                robot.rightDiff.setPower(0.5);
+            } else if (robot.posRightDiffy < intakeExposed - tolerance + differential) {
+                robot.rightDiff.setPower(-0.5);
+
+            } else {
+                robot.rightDiff.setPower(0);
+            }
+
+            if (engine.gamepad2.right_stick_x > 0) {
+                differential += 100;
+            } else if (engine.gamepad2.right_stick_x < 0) {
+                differential -= 100;
+            }
+        }
+
+        if (armPos.equals("Reset")) {
+            robot.intakeClaw.setPosition(IntakeCLOSE);
+            robot.depoClaw.setPosition(DepoOPEN);
+            robot.rightDiff.setPower(engine.gamepad2.right_stick_y);
+            robot.leftDiff.setPower(engine.gamepad2.left_stick_y);
+
+        }
+    }
+
+
+    @Override
+    public void init() {
+        robot.octoquad.resetAllPositions();
+
+    }
+
+    @Override
+    public void exec() {
+        robot.readOctoQuad();
+
+
+        // -------------------------------------------------------------------------------------------------------------- Player 1 (Wheels)
+        DriveTrainControl();
+
+        // ---------------------------------------------------------------------------------------------------------------- Player 2 (Toys)
+        robot.intakeExtendo.setPower(-engine.gamepad2.left_stick_y * 1);
+        robot.depositLeftExtendo.setPower(engine.gamepad2.right_trigger * 1);
+        robot.depositRightExtendo.setPower(engine.gamepad2.right_trigger * 1);
+
+        ArmPos();
+        ArmSequencer();
+
     }
 
     @Override
@@ -64,5 +274,11 @@ public class PatriotTeleOpState extends CyberarmState {
         engine.telemetry.addData("imu yaw", -robot.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
         engine.telemetry.addData("imu pitch", -robot.imu.getRobotYawPitchRollAngles().getPitch(AngleUnit.DEGREES));
         engine.telemetry.addData("imu roll", -robot.imu.getRobotYawPitchRollAngles().getRoll(AngleUnit.DEGREES));
+        engine.telemetry.addData("differential", differential);
+        engine.telemetry.addData("left diffy pos", robot.posLeftDiffy);
+        engine.telemetry.addData("right diffy pos", robot.posRightDiffy);
+        engine.telemetry.addData("armPos", armPos);
+        engine.telemetry.addData("max velo", maxVelocity);
+        engine.telemetry.addData("depo left servo pos", robot.depoLeft.getPosition());
     }
 }
