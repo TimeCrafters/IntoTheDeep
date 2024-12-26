cbe586b5e29c364d5de826bd38a966ff07e7598f
Cyberarm <matthewlikesrobots@gmail.com>
2024-12-26 16:48:27 -0600

    TACNET: attempt to launch service from installed TAC app before falling back to service bundled in FTC-RC app

--- TeamCode/src/main/java/org/timecrafters/tacnet_management/StartTACNETService.java
+++ TeamCode/src/main/java/org/timecrafters/tacnet_management/StartTACNETService.java
@@ -2,6 +2,7 @@ package org.timecrafters.tacnet_management;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -9,12 +10,25 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.timecrafters.TimeCraftersConfigurationTool.library.tacnet.TACNETServerService;
 
+import java.util.Objects;
+
 @TeleOp(name = "Start TACNET Service", group = "TACNET")
 public class StartTACNETService extends OpMode {
     @Override
     public void init() {
         Context appContext = hardwareMap.appContext;
-        Intent tacnetIntent = new Intent(appContext, TACNETServerService.class);
+        Intent tacnetIntent;
+        tacnetIntent = new Intent("org.timecrafters.TimeCraftersConfigurationTool.tacnet.ACTION_START_SERVER");
+        tacnetIntent.setPackage("org.timecrafters.TimeCraftersConfigurationTool");
+
+        // If TimeCrafters Configuration Tool app isn't installed then fallback to alternative means
+        // of running service that results in the service ending whenever the Robot Controller app
+        // is stopped or re-installed.
+        try {
+            appContext.getPackageManager().getPackageInfo(Objects.requireNonNull(tacnetIntent.getPackage()), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            tacnetIntent = new Intent(appContext, TACNETServerService.class);
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             appContext.startForegroundService(tacnetIntent);
