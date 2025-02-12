d4f836ae9bedb9aaaf408f9797541ba2d4fd818b
SpencerPiha <88354753+SpencerPiha@users.noreply.github.com>
2025-02-11 19:35:55 -0600

    TeleOp and robot stuff, arm is moving, servo's aren't

--- TeamCode/src/main/java/org/timecrafters/IntoTheDeep/common/PatriotMk2.java
+++ TeamCode/src/main/java/org/timecrafters/IntoTheDeep/common/PatriotMk2.java
@@ -4,9 +4,12 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.sun.tools.javac.comp.Todo;
 
 import org.timecrafters.Library.Robot;
 import org.timecrafters.TimeCraftersConfigurationTool.library.TimeCraftersConfiguration;
@@ -17,16 +20,85 @@ import dev.cyberarm.engine.V2.CyberarmEngine;
 public class PatriotMk2 implements Robot {
     private HardwareMap hardwareMap;
     public TimeCraftersConfiguration configuration;
+    public String TeleOrAuto = "tele";
     private String string;
     private CyberarmEngine engine;
+
+    // --------------------------------------------------------
+    // ----------------- Hardware Map -------------------------
+    // --------------------------------------------------------
     public DcMotorEx fl, fr, bl, br;
+    public DcMotorEx pivot, extensionLeft, extensionRight;
+
     public IMU imu;
 
-    // heading control
+    public Servo leftPivot, rightPivot, twist, claw;
+
+    // --------------------------------------------------------
+    // -------------- PID Controllers -------------------------
+    // --------------------------------------------------------
+
+
+    // -------------- Heading Control ------------------
     public double headingIntegralSum = 0;
-    public static double Hp = 0.7, Hi = 0, Hd = 0;
+    public static double Hp = 1, Hi = 0, Hd = 0;
     private double headingLastError = 0;
     ElapsedTime headingTimer = new ElapsedTime();
+
+    // X Axis Control ----------------------------------
+    public double xAxisIntegralSum = 0;
+    public static double Xp = 0.7, Xi = 0, Xd = 0;
+    private double xAxisLastError = 0;
+    ElapsedTime xAxisTimer = new ElapsedTime();
+
+    // Y Axis Control ----------------------------------
+    public double yAxisIntegralSum = 0;
+    public static double Yp = 0.7, Yi = 0, Yd = 0;
+    private double yAxisLastError = 0;
+    ElapsedTime yAxisTimer = new ElapsedTime();
+
+    // Extension Control -------------------------------
+    public int intakeFar = -600;
+    public int intakeClose = -100;
+    public int stow = 0;
+    public int basketHigh = -750;
+    public int basketLow = -300;
+
+    public double extensionIntegralSum = 0;
+    public static double Ep = 0.005, Ei = 0, Ed = 0;
+    private double extensionLastError = 0;
+    ElapsedTime extensionTimer = new ElapsedTime();
+
+    // Pivot Control -----------------------------------
+    public double pivotIntaking = -50;
+    public double pivotDepositing = 1200;
+    public double pivotMotorHover = 0;
+    public double pivotIntegralSum = 0;
+    public static double Pp = 0.00085, Pi = 0, Pd = 0;
+    private double pivotLastError = 0;
+    ElapsedTime pivotTimer = new ElapsedTime();
+
+    // ----------------------------------------------------------
+    // Automation Variables for arm -----------------------------
+    // ----------------------------------------------------------
+
+    public String armMode = "stowed";
+    public String servoMode = "stowed";
+    public double variableWristAngle;
+    public int oneTimeDeal = 0;
+    public long initTime = 0;
+    public long pickDelay = 0;
+    public double twistOrigin = 0.49;
+    public double twistPos = 0.49;
+    public double pivotPos = 0.6;
+    public double clawPos = 0.37;
+    public double pivotBasket = 0.6;
+    public double pivotBasketOut = 0.6;
+    public double pivotHover = 0.4;
+    public double pivotIntake = 0.15;
+    public double pivotStow = 0.6;
+    public double clawOpen = 0;
+    public double clawClose = 0.37;
 
     public PatriotMk2(String string) {
         this.engine = engine;
@@ -46,17 +118,49 @@ public class PatriotMk2 implements Robot {
         br = (DcMotorEx) engine.hardwareMap.dcMotor.get("br");
         bl = (DcMotorEx) engine.hardwareMap.dcMotor.get("bl");
 
+        pivot = (DcMotorEx) engine.hardwareMap.dcMotor.get("pivot");
+
+        extensionLeft = (DcMotorEx) engine.hardwareMap.dcMotor.get("sbl");
+        extensionRight = (DcMotorEx) engine.hardwareMap.dcMotor.get("sbr");
+
+        leftPivot = engine.hardwareMap.servo.get("lp");
+        rightPivot = engine.hardwareMap.servo.get("rp");
+        twist = engine.hardwareMap.servo.get("twist");
+        claw = engine.hardwareMap.servo.get("claw");
+
+        leftPivot.setDirection(Servo.Direction.FORWARD);
+        rightPivot.setDirection(Servo.Direction.REVERSE);
+
         fl.setDirection(DcMotorEx.Direction.REVERSE);
         fl.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         fr.setDirection(DcMotorEx.Direction.FORWARD);
         fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         bl.setDirection(DcMotorEx.Direction.FORWARD);
         bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         br.setDirection(DcMotorEx.Direction.FORWARD);
         br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        pivot.setDirection(DcMotorEx.Direction.REVERSE);
+        pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        extensionRight.setDirection(DcMotorEx.Direction.REVERSE);
+        extensionRight.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        extensionRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        extensionLeft.setDirection(DcMotorEx.Direction.REVERSE);
+        extensionLeft.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        extensionLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         IMU.Parameters parameters = new IMU.Parameters(
                 new RevHubOrientationOnRobot(
@@ -76,8 +180,12 @@ public class PatriotMk2 implements Robot {
         return radians;
     }
 
-    public double HeadingPIDControl(double reference, double current) {
-        double error = angleWrap(current - reference);
+    // -------------------------------------------------------------
+    // ------- PID Controllers for all automated functions ---------
+    // -------------------------------------------------------------
+
+    public double HeadingPIDControl(double target, double current) {
+        double error = angleWrap(current - target);
         headingIntegralSum += error * headingTimer.seconds();
         double derivative = (error - headingLastError) / headingTimer.seconds();
 
@@ -87,4 +195,183 @@ public class PatriotMk2 implements Robot {
         return output;
     }
 
+
+    public double XAxisPIDControl(double target, double current) {
+        double error = current - target;
+        xAxisIntegralSum += error * xAxisTimer.seconds();
+        double derivative = (error - xAxisLastError) / xAxisTimer.seconds();
+
+        xAxisTimer.reset();
+
+        double output = (error * Xp) + (derivative * Xd) + (xAxisIntegralSum * Xi);
+        return output;
+    }
+
+    public double YAxisPIDControl(double target, double current) {
+        double error = current - target;
+        yAxisIntegralSum += error * yAxisTimer.seconds();
+        double derivative = (error - yAxisLastError) / yAxisTimer.seconds();
+
+        yAxisTimer.reset();
+
+        double output = (error * Yp) + (derivative * Yd) + (yAxisIntegralSum * Yi);
+        return output;
+    }
+
+    public double ExtensionPIDControl(double target, double current) {
+        double error = current - target;
+        extensionIntegralSum += error * extensionTimer.seconds();
+        double derivative = (error - extensionLastError) / extensionTimer.seconds();
+
+        extensionTimer.reset();
+
+        double output = (error * Ep) + (derivative * Ed) + (extensionIntegralSum * Ei);
+        return output;
+    }
+
+    public double PivotPIDControl(double target, double current) {
+        double error = current - target;
+        pivotIntegralSum += error * pivotTimer.seconds();
+        double derivative = (error - pivotLastError) / pivotTimer.seconds();
+
+        pivotTimer.reset();
+
+        double output = (error * Pp) + (derivative * Pd) + (pivotIntegralSum * Pi);
+        return output;
+    }
+
+
+    // -------------------------------------------------------------
+    // ------- Arm Automation Modes And Control Presets ------------
+    // -------------------------------------------------------------
+
+    public void ArmModesPreset() {
+
+        int currentExtensionPos = extensionLeft.getCurrentPosition();
+        int currentPivotPos = pivot.getCurrentPosition();
+
+        switch (armMode) {
+            case "stowed":
+                extensionLeft.setPower(-ExtensionPIDControl(stow, currentExtensionPos));
+                extensionRight.setPower(-ExtensionPIDControl(stow, currentExtensionPos));
+                if (extensionLeft.getCurrentPosition() > -200) {
+                    pivot.setPower(PivotPIDControl(pivotIntaking, currentPivotPos));
+                }
+
+                break;
+            case "intake-close":
+                extensionLeft.setPower(-ExtensionPIDControl(intakeClose, currentExtensionPos));
+                extensionRight.setPower(-ExtensionPIDControl(intakeClose, currentExtensionPos));
+                pivot.setPower(PivotPIDControl(pivotIntaking, currentPivotPos));
+
+
+                break;
+            case "intake-far-hover":
+                extensionLeft.setPower(-ExtensionPIDControl(intakeFar, currentExtensionPos));
+                extensionRight.setPower(-ExtensionPIDControl(intakeFar, currentExtensionPos));
+                pivot.setPower(PivotPIDControl(pivotMotorHover, currentPivotPos));
+
+                break;
+            case "intake-far":
+                extensionLeft.setPower(-ExtensionPIDControl(intakeFar, currentExtensionPos));
+                extensionRight.setPower(-ExtensionPIDControl(intakeFar, currentExtensionPos));
+                pivot.setPower(PivotPIDControl(pivotIntaking, currentPivotPos));
+
+                break;
+            case "basket-high":
+                pivot.setPower(PivotPIDControl(pivotDepositing, currentPivotPos));
+                if (pivot.getCurrentPosition() > pivotDepositing - 300){
+                    extensionLeft.setPower(-ExtensionPIDControl(basketHigh, currentExtensionPos));
+                    extensionRight.setPower(-ExtensionPIDControl(basketHigh, currentExtensionPos));
+                }
+
+                break;
+            case "basket-low":
+                pivot.setPower(PivotPIDControl(pivotDepositing, currentPivotPos));
+                if (pivot.getCurrentPosition() > pivotDepositing - 300){
+                    extensionLeft.setPower(-ExtensionPIDControl(basketLow, currentExtensionPos));
+                    extensionRight.setPower(-ExtensionPIDControl(basketLow, currentExtensionPos));
+                }
+
+                break;
+        }
+    }
+
+    public void ServoModePreset() {
+
+        switch (servoMode) {
+            case "basket":
+                pivotPos = pivotBasket;
+                clawPos = clawClose;
+                twistPos = twistOrigin;
+                break;
+            case  "basket-out":
+                pivotPos = pivotBasketOut;
+                clawPos = clawOpen;
+                twistPos = twistOrigin;
+                break;
+            case "stowed-from-basket":
+                pivotPos = pivotStow;
+                clawPos = clawOpen;
+                twistPos = twistOrigin;
+                break;
+            case "pickup-far-hover-before":
+                pivotPos = pivotHover;
+                clawPos = clawOpen;
+                twistPos = twistOrigin;
+                break;
+            case "pickup-aim":
+                pivotPos = pivotIntake;
+                clawPos = clawOpen;
+
+                // TODO: 2/11/2025 Test this because it may not actually work.
+                if (TeleOrAuto.equals("tele")) {
+                    if (engine.gamepad1.x) {
+                        twistPos = twistOrigin;
+                    }
+
+                    if (engine.gamepad1.right_bumper) {
+                        twistPos = twistOrigin + 0.1;
+                    } else if (engine.gamepad1.left_bumper) {
+                        twistPos = twistOrigin - 0.1;
+                    }
+                } else if (TeleOrAuto.equals("auto")){
+                    twistPos = variableWristAngle;
+                }
+                oneTimeDeal = 0;
+
+                break;
+            case "pickup-shoot":
+
+                    clawPos = clawClose;
+
+                    if (oneTimeDeal == 0) {
+                        initTime = System.currentTimeMillis();
+                        oneTimeDeal += 1;
+                    }
+
+                    if (initTime + pickDelay < System.currentTimeMillis()) {
+                        servoMode = "pickup-far-hover-after";
+                    }
+
+                // TODO: 2/11/2025 Test this because quick release may not work.
+
+                break;
+            case "pickup-far-hover-after":
+                pivotPos = pivotHover;
+                clawPos = clawClose;
+                twistPos = twistOrigin;
+
+                oneTimeDeal = 0;
+
+                break;
+            case "stowed-from-pickup":
+
+                pivotPos = pivotStow;
+                clawPos = clawClose;
+                twistPos = twistOrigin;
+                break;
+
+        }
+    }
 }
--- TeamCode/src/main/java/org/timecrafters/IntoTheDeep/teleop/states/PatriotMk2TeleOpState.java
+++ TeamCode/src/main/java/org/timecrafters/IntoTheDeep/teleop/states/PatriotMk2TeleOpState.java
@@ -6,31 +6,21 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.timecrafters.IntoTheDeep.common.PatriotMk2;
 
-import dev.cyberarm.engine.V2.CyberarmState;
 
+import dev.cyberarm.engine.V2.CyberarmState;
 public class PatriotMk2TeleOpState extends CyberarmState {
 
     private final PatriotMk2 robot;
+
     private double drivePower = 1;
-    private double maxVelocity = 2700 * 0.85;
+    private double maxVelocity = 2680 * 0.90;
+
     private boolean lbsVar;
 
-    private double recordVelo;
     private double testPower = 0;
-    public boolean depositManualControl = false;
-    public double depositorArmPower = 2;
-//    public int intakeTarget = 0;
-    public int depositTarget = 0;
-//
-//    public int leftDifTarget = 0;
-//    public int rightDifTarget = 0;
-    public double maxExtendoVelo = 0;
     private double rx;
     private boolean headingLock;
     private double headingTarget;
-
-
-
 
     public PatriotMk2TeleOpState(PatriotMk2 robot) {
         this.robot = robot;
@@ -40,43 +30,45 @@ public class PatriotMk2TeleOpState extends CyberarmState {
 
         // -------------------------------------------------------------------------------------------------------------- Player 1 (Wheels)
 
-        if (engine.gamepad1.dpad_down) {
-            headingTarget = Math.toRadians(0);
-        } else if (engine.gamepad1.dpad_up) {
+        if (engine.gamepad1.right_stick_x != 0){
+            headingLock = false;
+        } if (engine.gamepad1.dpad_down) {
+            headingLock = true;
             headingTarget = Math.toRadians(180);
+        } else if (engine.gamepad1.dpad_up) {
+            headingLock = true;
+            headingTarget = Math.toRadians(0);
         } else if (engine.gamepad1.dpad_left) {
+            headingLock = true;
             headingTarget = Math.toRadians(90);
-        } else if (engine.gamepad1.dpad_right) {
+        } else if (engine.gamepad1.dpad_right || engine.gamepad1.x) {
+            headingLock = true;
             headingTarget = Math.toRadians(-90);
+        } else if (engine.gamepad1.y){
+            headingTarget = Math.toRadians(-45);
         }
 
         if (headingLock){
-            rx = robot.HeadingPIDControl(headingTarget, robot.imu.getRobotYawPitchRollAngles().getYaw());
+            rx = robot.HeadingPIDControl(headingTarget, robot.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
         } else {
             rx = engine.gamepad1.right_stick_x;
         }
 
         // reset orientation, and move camera when reset to see...
-        if (engine.gamepad1.b) {
+        if (engine.gamepad1.right_stick_button) {
             robot.imu.resetYaw();
         }
 
         // toggle to enter slow or fast mode
-        boolean a = engine.gamepad1.a;
-        if (a && !lbsVar) {
+        boolean lbs = engine.gamepad1.left_stick_button;
+        if (lbs && !lbsVar) {
             if (drivePower == 1) {
                 drivePower = 0.5;
             } else {
                 drivePower = 1;
             }
         }
-        lbsVar = a;
-
-        if (engine.gamepad1.left_trigger != 0){
-            testPower = 100000;
-        } else {
-            testPower = 0;
-        }
+        lbsVar = lbs;
 
         // math and variables for field oriented drive:
         // the variables that are used for the joystick math later on
@@ -102,17 +94,72 @@ public class PatriotMk2TeleOpState extends CyberarmState {
         robot.fr.setVelocity(frontRightPower * drivePower + testPower);
     }
 
+    public void ArmModes(){
+
+        if (engine.gamepad1.y && engine.gamepad1.left_trigger != 0){
+            robot.armMode = "basket-low";
+        }
+
+        if (engine.gamepad1.y && engine.gamepad1.left_trigger == 0 && robot.armMode.equals("stowed")){
+            robot.armMode = "basket-high";
+        }
+
+        if (engine.gamepad1.x && robot.armMode.equals("stowed")){
+            robot.armMode = "intake-far";
+        }
+
+        if (engine.gamepad1.b){
+            robot.armMode = "intake-close";
+        }
+
+        if (engine.gamepad1.a){
+            robot.armMode = "stowed";
+        }
+
+
+    }
+
+    public void ServoModes(){
+
+        robot.twist.setPosition(robot.twistPos);
+        robot.claw.setPosition(robot.clawPos);
+        robot.rightPivot.setPosition(robot.pivotPos);
+        robot.leftPivot.setPosition(robot.pivotPos);
+
+        if (robot.armMode.equals("basket-low") && engine.gamepad1.right_trigger == 0 || robot.armMode.equals("basket-high") && engine.gamepad1.right_trigger == 0){
+            robot.servoMode = "basket";
+        }
+        if (robot.armMode.equals("basket-low") && engine.gamepad1.right_trigger != 0 || robot.armMode.equals("basket-high") && engine.gamepad1.right_trigger != 0){
+            robot.servoMode = "basket-out";
+        }
+
+
+    }
+
+
+
     @Override
     public void init() {
+        robot.pivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.extensionLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.extensionRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.pivot.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        robot.extensionLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.extensionRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
     }
 
     @Override
     public void exec() {
-        if (recordVelo < robot.bl.getVelocity()){
-            recordVelo = robot.bl.getVelocity();
-        }
+        robot.TeleOrAuto = "tele";
+
         DriveTrainControl();
+        ArmModes();
+        ServoModes();
+        robot.ArmModesPreset();
+
     }
 
     @Override
@@ -121,7 +168,12 @@ public class PatriotMk2TeleOpState extends CyberarmState {
         engine.telemetry.addData("imu yaw", robot.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
         engine.telemetry.addData("imu pitch", robot.imu.getRobotYawPitchRollAngles().getPitch(AngleUnit.DEGREES));
         engine.telemetry.addData("imu roll", robot.imu.getRobotYawPitchRollAngles().getRoll(AngleUnit.DEGREES));
-        engine.telemetry.addData("Max Velocity", recordVelo);
+
+        engine.telemetry.addData("Heading lock enabled", headingLock);
+        engine.telemetry.addData("Heading target", headingTarget);
+
+        engine.telemetry.addData("Pivot Pos", robot.pivot.getCurrentPosition());
+        engine.telemetry.addData("extension Pos", robot.extensionLeft.getCurrentPosition());
 
     }
 
